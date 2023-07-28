d1784975317e36d6923f33bea423fe5e88ddc3a4
Juan Hernández Babón <jhbabon@github.com>
2023-07-28 16:17:51 +0200

    Fix ActiveRecord::SchemaCache#version so it uses connection#schema_version

--- activerecord/lib/active_record/connection_adapters/schema_cache.rb
+++ activerecord/lib/active_record/connection_adapters/schema_cache.rb
@@ -375,7 +375,7 @@ module ActiveRecord
       end
 
       def version(connection)
-        @version ||= connection.migration_context.current_version
+        @version ||= connection.schema_version
       end
 
       def schema_version
